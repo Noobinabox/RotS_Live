5bd6a1c4411b3958b16b549de5c6ee96c34babf4
drelidan7 <david.gurley@gmail.com>
2017-01-26 10:06:01 -0500

    Added "WaitList" class to wait functions.  Updates.
    Still working on rewriting the waiting code.  There are compile errors
    in this file, but it's not included in the project, so I'm not too
    worried about it.
    
    I updated the wait_functions to use a WaitList class (that will be a
    Singleton) and to use a std::list implementation instead of the custom
    implementation of a linked list that is currently being used.  This
    makes the code much easier to read.
    
    I still need to bring some functions over and/or create another light
    utility class that incorporates the missing functionality.

--- wait_functions.cpp
+++ wait_functions.cpp
@@ -1,13 +1,16 @@
 #include "base_utils.h"
+#include "char_utils.h"
 #include "wait_functions.h"
 #include "structs.h"
-#include "comm.h" // for delay functions
 
 namespace wait_functions
 {
 	//============================================================================
-	void start_wait(char_data* character, int cycle)
+	void WaitList::wait_state(char_data* character, int cycle)
 	{
+		if (!character)
+			return;
+
 		int command = 0;
 		int sub_command = 0;
 		int priority = 50;
@@ -21,18 +24,87 @@ namespace wait_functions
 	}
 
 	//============================================================================
-	void wait_state_brief(char_data* character, int cycle, int command, int sub_command, 
+	void WaitList::wait_state_brief(char_data* character, int cycle, int command, int sub_command, 
 		int priority, long affect_flag)
 	{
+		if (!character)
+			return;
+
 		wait_state_full(character, cycle, command, sub_command, priority, affect_flag,
 			0, NULL, affect_flag, TARGET_IGNORE);
 	}
 
+	// Adding this namespace to hold on to previous code that I plan
+	// on overwriting as a reference.
+	namespace CODE_SAVER
+	{
+		void add_char_to_waitlist(char_data* character, char_data* waiting_list)
+		{
+			/*
+			* bsantosus:  I kidnapped computer.
+			*  I love ROTS! It's the best game EVERS! <3
+			*  Dave and Brenda 4/14/2017! <3
+			*/
+
+			//TODO(dgurley):  Parse the function below to figure out what it is.
+			//                  I think it's a forward list just looking at it, but we'll see.
+			//TODO(dgurley):  Figure out a better way to handle waiting.
+			//
+			// It looks like it's a queue implementation.
+			// Keep track of last.
+			// This file will take over complete_delay and abort_delay.
+			// Make use of std::find to see if the character is in the waiting list.
+
+			char_data* waiting_list = NULL;
+			char_data* temp_character = waiting_list;
+
+			if (temp_character == character)
+			{
+				temp_character = character->delay.next;
+				waiting_list = temp_character;
+			}
+
+			// If there is no waiting list, the character is moved into the waiting list.
+			// If there is a waiting list, ensure that the character is at the end of it.
+
+			// All of this list wait stuff held in the character->.. ugh.
+			// This will be replaced by an appropriate data structure.
+			if (!temp_character)
+			{
+				waiting_list = character;
+			}
+			else
+			{
+				while (temp_character->delay.next)
+				{
+					if (temp_character->delay.next == character)
+					{
+						temp_character->delay.next = character->delay.next;
+					}
+					else
+					{
+						temp_character = temp_character->delay.next;
+					}
+				}
+
+				if (temp_character != character)
+				{
+					temp_character->delay.next = character;
+				}
+			}
+
+			character->delay.next = NULL;
+		}
+	}
+
 	//============================================================================
-	void wait_state_full(char_data* character, int cycle, int command, int sub_command,
-		int priority, int delay_flag, sh_int ch_num, void* argument, long affect_flag,
+	void WaitList::wait_state_full(char_data* character, int cycle, int command, int sub_command,
+		int priority, int delay_flag, signed short ch_num, void* argument, long affect_flag,
 		signed char data_type)
 	{
+		if (!character)
+			return;
+
 		if (character->delay.wait_value != 0)
 		{
 			if (priority > character->delay.priority)
@@ -62,59 +134,129 @@ namespace wait_functions
 		character->delay.targ2.type = TARGET_IGNORE;
 		base_utils::set_bit(character->specials.affected_by, affect_flag);
 
-		/*
-		 * bsantosus:  I kidnapped computer.
-		 *  I love ROTS! It's the best game EVERS! <3
-		 *  Dave and Brenda 4/14/2017! <3
-		 */
+		// This replicates the "add_char_to_waitlist" code, which is based on the waiting macro.
+		
+		// If the character is in the waiting list, remove him.
+		// Then insert the character at the end of the waiting list.
+		m_waitingList.erase(std::find(m_waitingList.begin(), m_waitingList.end(), character));
+		m_waitingList.push_back(character);
+	}
 
-		//TODO(dgurley):  Parse the function below to figure out what it is.
-		//                  I think it's a forward list just looking at it, but we'll see.
-		//TODO(dgurley):  Figure out a better way to handle waiting.
-		//
-		// It looks like it's a queue implementation.
-		// Keep track of last.
-		// This file will take over complete_delay and abort_delay.
-		// Make use of std::find to see if the character is in the waiting list.
+	//============================================================================
+	void WaitList::abort_delay(char_data* character)
+	{
+		if (!character)
+			return;
 
-		char_data* waiting_list = NULL;
-		char_data* temp_character = waiting_list;
+		// Update the character's wait values and priority.
+		base_utils::remove_bit(character->specials.affected_by, (long)AFF_WAITWHEEL);
+		base_utils::remove_bit(character->specials.affected_by, (long)AFF_WAITING);
+		character->delay.wait_value = 0;
+		character->delay.priority = 0;
 
-		if (temp_character == character)
+		// Remove the character from the waiting list.
+		m_waitingList.erase(std::find(m_waitingList.begin(), m_waitingList.end(), character));
+
+		// Ensure the character is not in the waiting list.
+		if (in_waiting_list(character)) 
 		{
-			temp_character = character->delay.next;
-			waiting_list = temp_character;
+			log("SYSERR: abort_delay, character remaining in waiting_list");
+			abort_delay(character);
+		}
+	}
+
+	//============================================================================
+	void WaitList::complete_delay(char_data *ch)
+	{
+		SPECIAL(*tmpfunc);
+
+		ch->delay.wait_value = 0;
+		REMOVE_BIT(ch->specials.affected_by, AFF_WAITWHEEL);
+		REMOVE_BIT(ch->specials.affected_by, AFF_WAITING);
+
+		if (ch->delay.cmd == CMD_SCRIPT) 
+		{
+			continue_char_script(ch);
+			return;
 		}
 
-		// If there is no waiting list, the character is moved into the waiting list.
-		// If there is a waiting list, ensure that the character is at the end of it.
-
-		// All of this list wait stuff held in the character->.. ugh.
-		// This will be replaced by an appropriate data structure.
-		if (!temp_character)
+		if (ch->delay.cmd == -1 && IS_NPC(ch)) 
 		{
-			waiting_list = character;
-		}
-		else
-		{
-			while (temp_character->delay.next)
+			/* Here calls special procedure */
+			if (mob_index[ch->nr].func)
 			{
-				if (temp_character->delay.next == character)
+				(*mob_index[ch->nr].func) (ch, 0, -1, "", SPECIAL_NONE, &(ch->delay));
+			}
+			else if (ch->specials.store_prog_number) 
+			{
+				tmpfunc = (SPECIAL(*)) virt_program_number(ch->specials.store_prog_number);
+				tmpfunc(ch, 0, ch->delay.cmd, "", SPECIAL_DELAY, &(ch->delay));
+			}
+			else if (ch->specials.union1.prog_number)
+			{
+				intelligent(ch, 0, -1, "", SPECIAL_DELAY, &(ch->delay));
+			}
+		}
+		else if (ch->delay.cmd > 0)
+		{
+			command_interpreter(ch, "", &(ch->delay));
+		}
+	}
+
+	//============================================================================
+	void WaitList::update()
+	{
+		// Process commands in the wait list.
+		for (auto char_iter = m_waitingList.begin(); char_iter != m_waitingList.end(); )
+		{
+			char_data* character = *char_iter;
+			int wait_value = character->delay.wait_value;
+			if (wait_value > 0)
+			{
+				wait_value = --character->delay.wait_value;
+			}
+
+			if (wait_value > 0)
+			{
+				if (!char_utils::is_npc(*character) && char_utils::is_affected_by(*character, AFF_WAITWHEEL))
 				{
-					temp_character->delay.next = character->delay.next;
+					if (char_utils::is_preference_flagged(*character, PRF_SPINNER))
+					{
+						// Add this function in somewhere.
+						//write_to_descriptor(wait_ch->desc->descriptor, wait_wheel[wait_value % 8]);
+					}
+				}
+
+				++char_iter;
+			}
+			else if (character->delay.wait_value == 0)
+			{
+				/* here is the block calling actual procedures */
+				complete_delay(character);
+				if (character->delay.wait_value == 0)
+				{
+					/* look out for the similar code in raw_kill() */
+					//abort_delay(character);
+					char_iter = m_waitingList.erase(char_iter);
 				}
 				else
 				{
-					temp_character = temp_character->delay.next;
+					++char_iter;
 				}
 			}
-
-			if (temp_character != character)
+			else
 			{
-				temp_character->delay.next = character;
+				// Are wait values of less than 0 desired/allowed?
+				// Unsure of what the purpose would be.  The character
+				// would stay in the list forever.
+				++char_iter;
 			}
 		}
+	}
 
-		character->delay.next = NULL;
+	//============================================================================
+	bool WaitList::in_waiting_list(char_data* character)
+	{
+		return std::find(m_waitingList.begin(), m_waitingList.end(), character) != m_waitingList.end();
 	}
 }
--- wait_functions.h
+++ wait_functions.h
@@ -1,17 +1,37 @@
 #pragma once
 
+#include <list>
+
 struct char_data;
 
 namespace wait_functions
 {
-	void start_wait(char_data& character, int cycle);
-	void wait_state_brief(char_data& character, int cycle);
-	void wait_state_full(char_data& character, int cycle);
-
-
-
-	namespace _INTERNAL
+	//TODO(dgurley):  Singleton implementation of this guy.
+	class WaitList
 	{
+	public:
+		void wait_state(char_data* character, int cycle);
+		void wait_state_brief(char_data* character, int cycle, 
+			int command, int sub_command, int priority, long affect_flag);
+		
+		void wait_state_full(char_data* character, int cycle, int command, 
+			int sub_command, int priority, int delay_flag, signed short ch_num, 
+			void* argument, long affect_flag, signed char data_type);
 
-	}
+		void abort_delay(char_data* wait_ch);
+		void complete_delay(char_data *ch);
+
+		/// Called once per game loop to go through the waiting list
+		/// and update its values.
+		void update();
+
+	private:
+		bool in_waiting_list(char_data *ch);
+
+	private:
+		//TODO(dgurley):  Use a more efficient data structure once you 
+		// figure out what the access pattern is.
+		std::list<char_data*> m_waitingList;
+
+	};
 }

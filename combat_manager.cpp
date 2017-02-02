31c058b6359f57bd414d29ece2e6196201505ab8
drelidan7 <David.Gurley@firaxis.com>
2017-02-02 17:06:43 -0500

    Combat Manager work.
    Adding more functionality to the combat manager to eventually take over
    the base attack cycle code in fight.cc

--- combat_manager.cpp
+++ combat_manager.cpp
@@ -1,18 +1,192 @@
 #include "combat_manager.h"
 
+#include "char_utils_combat.h"
+#include "char_utils.h"
+
+#include "utils.h"
+#include "spells.h"
+#include "comm.h"
+
 namespace game_rules
 {
+	/********************************************************************
+	* Public Functions
+	*********************************************************************/
+	//============================================================================
+	bool combat_manager::can_attack(char_data* attacker, char_data* victim)
+	{
+		if (!attacker || !victim)
+			return false;
+
+		if (attacker == victim)
+			return false;
+
+		// TODO(drelidan):  Put additional checks in here, such as sight, peace room,
+		// attacker and victim being colocated, etc.
+
+		return true;
+	}
+
+	//============================================================================
+	combat_manager::ob_roll combat_manager::roll_ob(char_data* attacker)
+	{
+		int roll = number(1, 35);
+
+		const room_data& attacker_room = m_world[attacker->in_room];
+		double attacker_OB = utils::get_real_ob(*attacker, m_weather, attacker_room);
+		attacker_OB += number(1, 55 + attacker_OB * 0.25);
+		attacker_OB += roll;
+		attacker_OB = attacker_OB * 7.0 / 8.0;
+		attacker_OB -= 40.0;
+
+		if (roll == 35)
+		{
+			attacker_OB += 100.0;
+		}
+
+		return ob_roll(roll, attacker_OB);
+	}
+
+	//============================================================================
+	bool combat_manager::is_hit_accurate(const char_data& attacker)
+	{
+		int tactics = utils::get_tactics(attacker);
+		
+		// Hits can only be accurate on careful or aggressive tactics.
+		if (tactics != TACTICS_CAREFUL && tactics != TACTICS_AGGRESSIVE)
+			return false;
+
+		int target_number = utils::get_prof_level(PROF_RANGER, attacker);
+		target_number -= utils::get_skill_penalty(attacker);
+		target_number -= utils::get_dodge_penalty(attacker);
+		target_number *= utils::get_skill(attacker, SKILL_ACCURACY) / 100;
+
+		int roll = number(0, 100);
+		return roll < target_number;
+	}
+
+	//============================================================================
+	double combat_manager::offense_if_weapon_hits(char_data* attacker, char_data* victim, int hit_type)
+	{
+		ob_roll rolled_ob = roll_ob(attacker);
+		
+		double attacker_ob = rolled_ob.get_ob_value();
+
+		int victim_position = victim->specials.position;
+		if (victim_position < POSITION_FIGHTING)
+		{
+			attacker_ob += 10 * (POSITION_FIGHTING - victim_position);
+		}
+
+		bool hit_accurate = is_hit_accurate(*attacker);
+		if (hit_accurate)
+		{
+			act("You manage to find an opening in $N's defense!", TRUE, attacker, NULL, victim, TO_CHAR);
+			act("$n notices an opening in your defense!", TRUE, attacker, NULL, victim, TO_VICT);
+		}
+		else
+		{
+			// Test for dodge and evasion.
+			double dodge_malus = utils::get_real_dodge(*victim);
+
+			attacker_ob -= dodge_malus;
+			if (attacker_ob < 0.0 && rolled_ob.get_rolled_value() != 35)
+			{
+				// Victim dodged the attack.
+				act("$N dodges $n's attack.", FALSE, attacker, 0, victim, TO_NOTVICT, TRUE);
+				act("$N dodges your attack.", FALSE, attacker, 0, victim, TO_CHAR, TRUE);
+				act("You dodge $n's attack.", FALSE, attacker, 0, victim, TO_VICT, TRUE);
+				return attacker_ob;
+			}
+
+			double evasion_malus = get_evasion_malus(*attacker, *victim);
+
+			attacker_ob -= evasion_malus;
+			if (attacker_ob < 0.0 && rolled_ob.get_rolled_value() != 35)
+			{
+				// Victim evaded the attack.
+				act("$N distracts $n into missing $M.", FALSE, attacker, 0, victim, TO_NOTVICT, TRUE);
+				act("$N is not where you attack $M.", FALSE, attacker, 0, victim, TO_CHAR, TRUE);
+				act("You evade $n's attack.", FALSE, attacker, 0, victim, TO_VICT, TRUE);
+				return attacker_ob;
+			}
+		}
+
+		// Test for parry.
+		if (victim_position > POSITION_STUNNED)
+		{
+			const room_data& room = m_world[victim->in_room];
+			double victim_parry = utils::get_real_parry(*victim, m_weather, room);
+			victim_parry *= victim->specials.current_parry * 0.01;
+
+			// Apply a parry malus to the victim.
+			victim->specials.current_parry = victim->specials.current_parry * 2 / 3;
+
+			attacker_ob -= victim_parry;
+			if (rolled_ob.get_rolled_value() == 35)
+			{
+				// Attackers can't get below 0 OB with a roll of 35.
+				attacker_ob = std::max(attacker_ob, 0.0);
+			}
+
+			if (attacker_ob < 0.0)
+			{
+				act("$N deflects $n's attack.", FALSE, attacker, 0, victim, TO_NOTVICT, TRUE);
+				act("$N deflects your attack.", FALSE, attacker, 0, victim, TO_CHAR, TRUE);
+				act("You deflect $n's attack.", FALSE, attacker, 0, victim, TO_VICT, TRUE);
+
+				//TODO(dgurley):  Add Riposte check in here.
+				/*
+				if (check_riposte(ch, victim))
+					return;
+
+				check_grip(ch, wielded);
+				check_grip(victim, victim->equipment[WIELD]);
+				*/
+			}
+		}
+
+		return attacker_ob;
+	}
+
+	//============================================================================
+	double combat_manager::get_evasion_malus(const char_data& attacker, const char_data& victim)
+	{
+		if (!utils::is_affected_by(victim, AFF_EVASION))
+			return 0.0;
+
+		double evasion_malus = 5.0 + utils::get_prof_level(PROF_CLERIC, victim) * (100 - utils::get_perception(attacker)) * 0.005;
+		return evasion_malus;
+	}
+	
+	//============================================================================
+	void combat_manager::on_weapon_hit(char_data* attacker, char_data* victim, int hit_type)
+	{
+
+	}
+
+	//============================================================================
+	void combat_manager::apply_damage(char_data* victim, double damage)
+	{
+
+	}
+
+
+
 	/********************************************************************
 	* Singleton Implementation
 	*********************************************************************/
 	combat_manager* combat_manager::m_pInstance(0);
 	bool combat_manager::m_bDestroyed(false);
 
-	combat_manager::combat_manager()
+	//============================================================================
+	combat_manager::combat_manager(const weather_data& weather, const room_data* world) :
+		m_weather(weather), m_world(world)
 	{
 
 	}
 
+	//============================================================================
 	combat_manager::~combat_manager()
 	{
 
--- combat_manager.h
+++ combat_manager.h
@@ -2,6 +2,10 @@
 
 #include "base_utils.h"
 
+struct char_data;
+struct weather_data;
+struct room_data;
+
 namespace game_rules
 {
 	class combat_manager
@@ -9,10 +13,10 @@ namespace game_rules
 	public:
 
 		// Create a new Singleton and store a pointer to it in m_pInstance.
-		static void create()
+		static void create(const weather_data& weather, const room_data* world)
 		{
 			// Task: initialize m_pInstance.  Ensure there is only one.
-			static combat_manager theInstance;
+			static combat_manager theInstance(weather, world);
 			m_pInstance = &theInstance;
 		}
 
@@ -33,6 +37,36 @@ namespace game_rules
 			return *m_pInstance;
 		}
 
+		// Structure to hold the OB Roll data.
+		struct ob_roll
+		{
+		public:
+			ob_roll() : m_rolled_value(0), m_ob_value(0.0) { }
+			ob_roll(int roll, double ob) : m_rolled_value(roll), m_ob_value(ob) { }
+
+			int get_rolled_value() { return m_rolled_value; }
+			double get_ob_value() { return m_ob_value; }
+
+		private:
+			int m_rolled_value;
+			double m_ob_value;
+		};
+
+		ob_roll roll_ob(char_data* attacker);
+
+		bool can_attack(char_data* attacker, char_data* victim);
+		bool is_hit_accurate(const char_data& attacker);
+
+		// This will return the character's 'left-over' OB if their strike hits.  If this value
+		// is less than 1, then the character missed.
+		double offense_if_weapon_hits(char_data* attacker, char_data* victim, int hit_type);
+
+		double get_evasion_malus(const char_data& attacker, const char_data& victim);
+
+		void on_weapon_hit(char_data* attacker, char_data* victim, int hit_type);
+
+		void apply_damage(char_data* victim, double damage);
+
 	private:
 		// Deleted functions.
 		// Don't put definitions in here so trying to do them will cause a compile error.
@@ -40,12 +74,15 @@ namespace game_rules
 		combat_manager& operator=(const combat_manager&);
 
 		/// Constructor/Destructor
-		combat_manager();
+		combat_manager(const weather_data& weather, const room_data* world);
 		~combat_manager();
 
 		/// Instance of the singleton, and lifetime management.
 		static combat_manager* m_pInstance;
 		static bool m_bDestroyed;
+
+		const weather_data& m_weather;
+		const room_data* m_world;
 	};
 }
 

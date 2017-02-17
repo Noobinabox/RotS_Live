04335220aafd16fa485f614da1a7d105f787732f
drelidan7 <David.Gurley@firaxis.com>
2017-02-17 12:54:26 -0500

    Work on Big Brother.

--- big_brother.cpp
+++ big_brother.cpp
@@ -12,6 +12,80 @@ bool game_rules::big_brother::m_bDestroyed(false);
 
 namespace game_rules
 {
+	//============================================================================
+	// Query Code:
+	//   This code is responsible for answering yes/no to questions.
+	//============================================================================ 
 	
+
+	//============================================================================
+	// Data Structure Management Code:
+	//   This code is responsible for ensuring that our data structures stay up-to-date.
+	//============================================================================ 
+	
+	//============================================================================
+	void big_brother::on_character_attacked_player(const char_data* attacker)
+	{
+		// Get the current time.
+		time_t current_time;
+		time(&current_time);
+
+		tm* time_info = localtime(&current_time);
+		m_last_engaged_pk_time[attacker] = *time_info; // copy tm_struct into map
+	}
+
+	//============================================================================
+	void big_brother::on_character_afked(const char_data* character)
+	{
+		typedef std::map<const char_data*, tm>::iterator map_iter;
+
+		map_iter attack_time = m_last_engaged_pk_time.find(character);
+		if (attack_time != m_last_engaged_pk_time.end())
+		{
+			// The character has engaged in PK.  How long ago was it?
+			time_t current_time;
+			time(&current_time);
+
+			tm cooldown;
+			cooldown.tm_min = 15;
+			cooldown.tm_sec = attack_time->second;
+
+
+		}
+
+		m_afk_characters.insert(character);
+	}
+
+	//============================================================================
+	void big_brother::on_character_returned(const char_data* character)
+	{
+		remove_character_from_set(character);
+	}
+
+	//============================================================================
+	void big_brother::on_character_disconnected(const char_data* character)
+	{
+		remove_character_from_set(character);
+
+		typedef std::map<const char_data*, time_t>::iterator map_iter;
+
+		map_iter char_map_iter = m_last_engaged_pk_time.find(character);
+		if (char_map_iter != m_last_engaged_pk_time.end())
+		{
+			m_last_engaged_pk_time.erase(char_map_iter);
+		}
+	}
+
+	//============================================================================
+	void big_brother::remove_character_from_set(const char_data* character)
+	{
+		typedef std::set<const char_data*>::iterator iter;
+
+		iter char_iter = m_afk_characters.find(character);
+		if (char_iter != m_afk_characters.end())
+		{
+			m_afk_characters.erase(character);
+		}
+	}
 }
 
--- big_brother.h
+++ big_brother.h
@@ -5,6 +5,7 @@
 
 #include <map>
 #include <set>
+#include <time.h>
 
 struct obj_data;
 struct char_data;
@@ -15,8 +16,8 @@ namespace game_rules
 	{
 	public:
 
-		// Called before any character loots a corpse.  This enforces our PK loot rules.
-		bool on_loot_item(char_data* looter, obj_data* corpse);
+		// Called before any character loots an item.  This enforces our PK loot rules.
+		bool on_loot_item(char_data* looter, obj_data* item);
 
 		// Called before any character attempts to damage or attack another character.
 		// This enforces our PK engagement rules.
@@ -31,11 +32,22 @@ namespace game_rules
 		void on_character_died(char_data* character, obj_data* corpse);
 
 		// Called when a character auto-AFKs so that they can [potentially] be protected.
-		void on_character_afked(char_data* character);
+		void on_character_afked(const char_data* character);
 
 		// Called when a character successfully attacks another player.  This is used to
 		// track whether or not a character should get AFK protection.
-		void on_character_attacked_player(char_data* attacker);
+		void on_character_attacked_player(const char_data* attacker);
+
+		// When a character disconnects, let the Big Brother system know so that it can
+		// clean up any references.
+		void on_character_disconnected(const char_data* character);
+
+		// When a corpse decays, let the Big Brother system know so that it will no longer
+		// track it.
+		void on_corpse_decayed(obj_data* corpse);
+
+		// Alert the Big Brother system that a character has returned to their keyboard.
+		void on_character_returned(const char_data* character);
 
 	private:
 
@@ -51,12 +63,18 @@ namespace game_rules
 		// Returns true if the victim has been auto-afk'd.
 		bool is_target_afk(const char_data* victim);
 
+		// Removes a character from our afk_characters set.
+		void remove_character_from_set(const char_data* character);
+
 		// TODO(drelidan):
 		// There will be a map between obj_data* corpses and a struct containing information
 		// about them - who they belong to, who has looting rights, etc.
+		std::map<obj_data*, char_data*> m_corpse_map;
 
-		// There will be a set of characters that have been auto-afk'd.
-
+		std::set<const char_data*> m_afk_characters;
+		
+		// For tracking when people engaged in PK can get AFK protection.
+		std::map<const char_data*, tm> m_last_engaged_pk_time;
 	};
 }
 

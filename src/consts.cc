8706f3e1d0ece4691141464b085bf794c57060c0
Noobinabox <slyon02@gmail.com>
2019-09-11 19:31:50 -0500

    Set starting room for haradrims.

--- src/consts.cc
+++ src/consts.cc
@@ -3176,7 +3176,7 @@ int mortal_start_room[MAX_RACES] = {
     13626, // Uruk-Lhuth
     0, // !NPC - Undead!
     10263, // Olog-Hai
-    6614, // Haradrim
+    6604, // Haradrim
     0, // !UNUSED!
     0, // !NPC - Troll!
     0 // !UNUSED!

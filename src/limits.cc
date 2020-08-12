7b776ad7b95be62082069dbcfade28a57bdaca85
drelidan7 <david.gurley@gmail.com>
2020-08-11 21:53:59 -0400

    Allow move regen spells to effect mounts/tames again.

--- src/limits.cc
+++ src/limits.cc
@@ -282,7 +282,7 @@ float move_gain(const char_data* character)
     const char_ability_data& stats = character->tmpabilities;
     if (is_npc(*character)) {
         if (is_mob_flagged(*character, MOB_MOUNT)) {
-            return 26 + (character->player.level * 2) + stats.con + stats.dex;
+            return 26 + (character->player.level * 2) + stats.con + stats.dex + get_bonus_move_gain(character);
         }
     }
 
@@ -324,7 +324,8 @@ float move_gain(const char_data* character)
                 gain *= 2.0;
             }
         }
-        return int(gain - 5);
+        gain += get_bonus_move_gain(character);
+        return gain - 5;
     } else {
         int race = character->player.race;
         if (race == RACE_WOOD || race == RACE_HIGH) {

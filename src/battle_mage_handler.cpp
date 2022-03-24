440c2f2c06c05d2b2c351c13cd09b093de6df956
Noobinabox <slyon02@gmail.com>
2022-03-24 09:45:52 -0500

    Format battle_mage_handler.cpp

--- src/battle_mage_handler.cpp
+++ src/battle_mage_handler.cpp
@@ -6,86 +6,77 @@ namespace player_spec {
 //============================================================================
 // Battle Mage Implementation
 //============================================================================
-battle_mage_handler::battle_mage_handler(const char_data* in_character)
-{
-    is_battle_spec = utils::get_specialization(*in_character) == game_types::PS_BattleMage;
-    tactics = in_character->specials.tactics;
-    mage_level = utils::get_prof_level(PROF_MAGE, *in_character);
-    warrior_level = utils::get_prof_level(PROF_WARRIOR, *in_character);
-}
-
-int battle_mage_handler::get_bonus_spell_pen(int spell_pen) const
-{
-    if (!is_battle_spec) {
-        return spell_pen;
+    battle_mage_handler::battle_mage_handler(const char_data *in_character) {
+        is_battle_spec = utils::get_specialization(*in_character) == game_types::PS_BattleMage;
+        tactics = in_character->specials.tactics;
+        mage_level = utils::get_prof_level(PROF_MAGE, *in_character);
+        warrior_level = utils::get_prof_level(PROF_WARRIOR, *in_character);
     }
 
-    return spell_pen + (tactics / 2) + (mage_level / 12);
-}
+    int battle_mage_handler::get_bonus_spell_pen(int spell_pen) const {
+        if (!is_battle_spec) {
+            return spell_pen;
+        }
 
-int battle_mage_handler::get_bonus_spell_power(int spell_power) const
-{
-    if (!is_battle_spec) {
-        return spell_power;
+        return spell_pen + (tactics / 2) + (mage_level / 12);
     }
 
-    return spell_power + (tactics / 2) + (mage_level / 12);
-}
+    int battle_mage_handler::get_bonus_spell_power(int spell_power) const {
+        if (!is_battle_spec) {
+            return spell_power;
+        }
 
-bool battle_mage_handler::can_prepare_spell() const
-{
-    return !is_battle_spec;
-}
-
-bool battle_mage_handler::does_spell_get_interrupted() const
-{
-    if (!is_battle_spec) {
-        return true;
+        return spell_power + (tactics / 2) + (mage_level / 12);
     }
 
-    if (tactics < TACTICS_AGGRESSIVE) {
-        return number() > base_chance;
+    bool battle_mage_handler::can_prepare_spell() const {
+        return !is_battle_spec;
     }
 
+    bool battle_mage_handler::does_spell_get_interrupted() const {
+        if (!is_battle_spec) {
+            return true;
+        }
 
-    float warrior_bonus = (float)warrior_level / 100.0f;
-    float mage_bonus = (float)mage_level / 100.0f;
-    float tactic_bonus = (float)(tactics * 2) / 100.0f;
-    float total_bonus = base_chance + warrior_bonus + mage_bonus + tactic_bonus;
-    return number() > total_bonus;
-}
+        if (tactics < TACTICS_AGGRESSIVE) {
+            return number() > base_chance;
+        }
 
-bool battle_mage_handler::does_mental_attack_interrupt_spell() const
-{
-  if (!is_battle_spec)
-  {
-    return true;
-  }
 
-  if (tactics < TACTICS_AGGRESSIVE)
-  {
-    return number() > base_chance;
-  }
-
-  float mage_bonus = (float)mage_level / 100.0f;
-  float tactic_bonus = (float)(tactics * 2) / 100.0f;
-  float total_bonus = base_chance + mage_bonus + tactic_bonus;
-  return number() > total_bonus;
-}
-
-bool battle_mage_handler::does_armor_fail_spell() const
-{
-    if (!is_battle_spec) {
-        return true;
+        float warrior_bonus = (float) warrior_level / 100.0f;
+        float mage_bonus = (float) mage_level / 100.0f;
+        float tactic_bonus = (float) (tactics * 2) / 100.0f;
+        float total_bonus = base_chance + warrior_bonus + mage_bonus + tactic_bonus;
+        return number() > total_bonus;
     }
 
-    if (tactics < TACTICS_AGGRESSIVE) {
-        return number() > base_chance;
+    bool battle_mage_handler::does_mental_attack_interrupt_spell() const {
+        if (!is_battle_spec) {
+            return true;
+        }
+
+        if (tactics < TACTICS_AGGRESSIVE) {
+            return number() > base_chance;
+        }
+
+        float mage_bonus = (float) mage_level / 100.0f;
+        float tactic_bonus = (float) (tactics * 2) / 100.0f;
+        float total_bonus = base_chance + mage_bonus + tactic_bonus;
+        return number() > total_bonus;
     }
 
-    float tactic_bonus = (float)(tactics * 2) / 100.0f;
-    float warrior_bonus = (float)warrior_level / 100.0f;
-    float total_bonus = base_chance + tactic_bonus + warrior_bonus;
-    return number() > total_bonus;
-}
+    bool battle_mage_handler::does_armor_fail_spell() const {
+        if (!is_battle_spec) {
+            return true;
+        }
+
+        if (tactics < TACTICS_AGGRESSIVE) {
+            return number() > base_chance;
+        }
+
+        float tactic_bonus = (float) (tactics * 2) / 100.0f;
+        float warrior_bonus = (float) warrior_level / 100.0f;
+        float total_bonus = base_chance + tactic_bonus + warrior_bonus;
+        return number() > total_bonus;
+    }
 } // namespace player_spec

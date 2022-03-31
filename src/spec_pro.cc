2aade9fcf070309b7a8d08656944f11709b94312
Noobinabox <slyon02@gmail.com>
2022-03-31 15:18:11 -0500

    Update and remove libraries for spec_proc.cc

--- src/spec_pro.cc
+++ src/spec_pro.cc
@@ -9,10 +9,9 @@
 ************************************************************************ */
 
 #include "platdef.h"
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <vector>
 
 #include "comm.h"

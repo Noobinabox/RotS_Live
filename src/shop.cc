7f44af2fce8a660f8a57a28d258a5e188633f96a
Noobinabox <slyon02@gmail.com>
2022-03-31 15:13:46 -0500

    Update and remove libraries for shop.cc

--- src/shop.cc
+++ src/shop.cc
@@ -9,9 +9,9 @@
 ************************************************************************ */
 
 #include "platdef.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 #include "comm.h"
 #include "db.h"

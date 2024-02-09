a7c569b8b0f6f970d759527d2752d61f957ed2e1
Orkbolg <voidport@protonmail.com>
2024-02-09 11:37:51 -0600

    Added messaging for mass spells and updated prayer table.

--- src/mystic.cpp
+++ src/mystic.cpp
@@ -27,7 +27,6 @@
 #include "structs.h"
 #include "utils.h"
 #include <assert.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,7 +34,6 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <vector>
 
 /*---------------------------------------------------------------------------------------------*/
 
@@ -983,7 +981,7 @@ void cast_mass_spell(char_data* caster, void (*spell)(char_data* caster, char* a
 
     for (auto iter = caster->group->begin(); iter != caster->group->end(); ++iter) {
         char_data* group_member = *iter;
-        if (group_member != caster && group_member->in_room == caster->in_room) {
+        if (group_member->in_room == caster->in_room) {
             spell(caster, nullptr, SPELL_TYPE_SPELL, group_member, nullptr, 0, 0);
         }
     }
@@ -991,25 +989,28 @@ void cast_mass_spell(char_data* caster, void (*spell)(char_data* caster, char* a
 
 ASPELL(spell_mass_regeneration) {
     if (utils::get_skill(*caster, SPELL_REGENERATION) < 100) {
-        send_to_char("Only those who have mastered regeneration may cast this spell", caster);
+        send_to_char("Only those who have mastered regeneration may cast this spell.\n\r", caster);
         return;
     }
+    send_to_room("The air hums as a powerful magic breathes life into all who stand within its embrace.\n\r", caster->in_room);
     cast_mass_spell(caster, spell_regeneration);
 }
 
 ASPELL(spell_mass_vitality) {
     if (utils::get_skill(*caster, SPELL_VITALITY) < 100) {
-        send_to_char("Only those who have mastered vitality may cast this spell", caster);
+        send_to_char("Only those who have mastered vitality may cast this spell.\n\r", caster);
         return;
     }
+    send_to_room("The air hums as a powerful magic breathes vitality into all who stand within its embrace.\n\r", caster->in_room);
     cast_mass_spell(caster, spell_vitality);
 }
 
 ASPELL(spell_mass_insight) {
     if (utils::get_skill(*caster, SPELL_INSIGHT) < 100) {
-        send_to_char("Only those who have mastered insight may cast this spell", caster);
+        send_to_char("Only those who have mastered insight may cast this spell.\n\r", caster);
         return;
     }
+    send_to_room("The air hums as a powerful magic brings insight into all who stand within its embrace.\n\r", caster->in_room);
     cast_mass_spell(caster, spell_insight);
 }
 

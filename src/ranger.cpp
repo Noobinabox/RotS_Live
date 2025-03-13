c96254b940cbc8af9465e2e3b865d1830f18038c
ahumbert <a.humbert@tukaiz.com>
2025-03-12 21:03:53 -0500

    readability

--- src/ranger.cpp
+++ src/ranger.cpp
@@ -1496,10 +1496,10 @@ ACMD(do_tame)
         victim = (struct char_data*)wtl->targ1.ptr.ch;
     }
 
-    double rl = (double)GET_PROF_LEVEL(PROF_RANGER, ch) / 3;
-    double tmm = (double)tame_skill / 30;
-    double res = rl + tmm;
-    levels_over_required = (int)res - GET_LEVEL(victim) - get_followers_level(ch);
+    double r_lvl = (double)GET_PROF_LEVEL(PROF_RANGER, ch) / 3;
+    double tm_skl = (double)tame_skill / 30;
+    double skl_total = r_lvl + tm_skl;
+    levels_over_required = (int)skl_total - GET_LEVEL(victim) - get_followers_level(ch);
 
     if (affected_by_spell(victim, SKILL_CALM))
         levels_over_required += 1;
